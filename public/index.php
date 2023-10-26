37d74040481945df911771c31b373a44dbdf8520
Bosun <beliy-81@mail.ru>
2023-10-26 19:59:06 +0300

    update project

--- public/index.php
+++ public/index.php
@@ -5,13 +5,60 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
 
-$app = AppFactory::create();
-$app->addErrorMiddleware(true, true, true);
-
+//$app = AppFactory::create();
+//$app->addErrorMiddleware(true, true, true);
+/*
 $app->get('/', function ($request, $response) {
     $response->getBody()->write('Welcome to Slim!');
     return $response;
     // Благодаря пакету slim/http этот же код можно записать короче
     // return $response->write('Welcome to Slim!');
 });
+*/
+
+//$app = AppFactory::create();
+//
+//$app->get('/users', function ($request, $response) {
+//    return $response->write('GET /users');
+//});
+
+//$app->post('/users', function ($request, $response) {
+//    return $response->write('POST /users');
+//});
+
+
+//$app->run();
+
+/*
+$faker = \Faker\Factory::create();
+$faker->seed(1234);
+
+$domains = [];
+for ($i = 0; $i < 10; $i++) {
+    $domains[] = $faker->domainName;
+}
+
+$phones = [];
+for ($i = 0; $i < 10; $i++) {
+    $phones[] = $faker->phoneNumber;
+}
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    return $response->write('go to the /phones or /domains');
+});
+
+// BEGIN (write your solution here)
+$app->get('/phones', function ($request, $response) use ($phones) {
+    return $response->write(json_encode($phones));
+});
+
+$app->get('/domains', function ($request, $response) use ($domains) {
+    return $response->write(json_encode($domains));
+});
+// END
+
 $app->run();
+*/
